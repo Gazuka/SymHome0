b9e9d905669bb2c796e8c62c313e70c471ee7426
Gazuka <jerome.carion@gmail.com>
2019-03-29 20:04:31 +0100

    Home v0.1.24

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,40 +8,78 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $Json;
+    private $Action;
+    private $Speech = "Désolé je ne sais quoi répondre...";
+
     /**
      * @Route("/home", name="home")
      */
     public function index()
     {
         $method = $_SERVER['REQUEST_METHOD'];
-        // Process only when method is POST
-        $speech = "rien";
-        
-        if($method == 'POST')
-        {
-	        //Récuperation du json
-	        $requestBody = file_get_contents('php://input');
-            $json = json_decode($requestBody);
-            //Récupération d'une variable dans le json
-            $action = $json->queryResult->action;
-            $age = $json->queryResult->outputContexts->parameters[1]->age;
-            //$age = "5";
-            
-            $speech = $this->$action($age);
-        }
-        
-        //$speech = "coucou jerome merci".$action;
-        //Répondre au Google Home
-	    $response = new \stdClass();
-	    $response->fulfillmentText = $speech;
-	    //$response->displayText = $speech;
-	    //$response->source = "webhook";
-        $home = json_encode($response);
-        return new Response($home);
-        //return $this->render('home/index.html.twig');
+        if($method == 'POST'){$fichier = 'php://input';}else{$fichier = 'test.json';}//fichier reel ou fichier fictif pour dev
+
+        //Récupération du fichier Json
+        $this->fulfillment('test.json');
+        //Appel de la fonction demandé
+        $this->Speech = $this->fulfillmentTransformAction();
+
+        return $this->fulfillmentRepondre();        
     }
 
-    public function manger($age) {
+    private function manger() {
+        $age = $this->fulfillmentRecupContext('patate', 'age');
         return "Je mange ". $age;
+    }
+
+    /**
+     * Permet de récupérer le Json d'un Fulfillment DialogFlow
+     */
+    private function fulfillment($fichier)
+    {
+        //Récuperation du json
+        $requestBody = file_get_contents($fichier);
+        $json = json_decode($requestBody);
+        $this->Json = $json;
+
+        //Récupération de la variable Action dans le json
+        $this->Action = $this->Json->queryResult->action;
+    }
+    private function fulfillmentAction()
+    {
+        
+    }
+
+    private function fulfillmentTransformAction()
+    {
+        switch($this->Action)
+        {
+            case 'manger':
+                return $this->manger();
+            break;
+        }
+    }
+
+    private function fulfillmentRecupContext($context, $variable)
+    {
+        foreach($this->Json->queryResult->outputContexts as $param)   
+        {
+            $tab = explode("/", $param->name);
+            $ContextName = $tab[count($tab)-1];
+            if($context == $ContextName)
+            {
+                return $param->parameters->$variable;
+            }            
+        } 
+    }
+
+    private function fulfillmentRepondre()
+    {
+        //Répondre au Google Home
+        $response = new \stdClass();
+        $response->fulfillmentText = $this->Speech;
+        $home = json_encode($response);
+        return new Response($home);
     }
 }
